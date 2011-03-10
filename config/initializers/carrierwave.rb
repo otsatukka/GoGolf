541b8c8d34a1c1c162f05c754d8eb1bd9c1f69dd
GoGolf <gogolf@gogolf.fi>
2011-03-10 04:08:21 +0200

    more more

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,6 +2,7 @@ CarrierWave.configure do |config|
   config.s3_access_key_id = 'AKIAIRZHHM7NFZ7WBM4A'
   config.s3_secret_access_key = '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'
   config.s3_bucket = 'gogolfprod'
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
   #config.s3_access_policy = :public_read
   #config.s3_headers = {'Cache-Control' => 'max-age=315576000'}
   #config.s3_region = 'eu-west-1'
