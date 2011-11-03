a769c98913046dcb14309f3ea925fc1140289d2d
otsatukka <mikko@gogolf.fi>
2011-11-03 23:25:35 +0200

    uusi oiste

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,9 @@ CarrierWave.configure do |config|
   config.s3_access_key_id = 'AKIAIRZHHM7NFZ7WBM4A'
   config.s3_secret_access_key = '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'
   config.s3_bucket = 'gogolfprod'
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
+  #config.cache_dir = "#{Rails.root}/tmp/uploads"
+  config.root = Rails.root.join('tmp')
+  config.cache_dir = 'carrierwave'
   #config.s3_access_policy = :public_read
   #config.s3_headers = {'Cache-Control' => 'max-age=315576000'}
   #config.s3_region = 'eu-west-1'
